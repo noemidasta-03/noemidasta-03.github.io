8a4527e14ef39cde13a8c16594609d912b4e3964
noemidasta-03 <noemi.dasta18@gmail.com>
2024-12-20 18:59:57 +0100

    create portfolio

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Presentation from "./components/Presentation";
+
+function App() {
+  return (
+    <>
+      <Presentation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Presentation.tsx
@@ -0,0 +1,22 @@
+import "./Presentation.css";
+import fotonoemi from "./foto-noemi.png";
+export default function Presentation() {
+  return (
+    <div className="container">
+      <div className="introduction">
+        <h1>Ciao, sono Noemi D'Asta</h1>
+        <div className="description">
+          <strong>Junior Full Stack Web Developer</strong>
+          <p>
+            con esperienza in HTML, CSS, JavaScript, React, Node.js e SQL. Sono
+            appassionata di tecnologia, problem solving e sempre alla ricerca di
+            nuove sfide per crescere e perfezionarmi come sviluppatrice.
+          </p>
+        </div>
+        <div>
+          <img src={fotonoemi} className="style-img" />
+        </div>
+      </div>
+    </div>
+  );
+}
